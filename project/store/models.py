437f77bf1ab4f5455b39a8e03b507260ffb8c52b
Aleksey Konotop <alekseykonotop@gmail.com>
2019-09-24 00:34:50 +0300

    Added models.py to commit and changed version of django in requirements.txt.

--- project/store/models.py
+++ project/store/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=150, verbose_name='Название категории')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['-name', ]
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=350, verbose_name='Заголовок')
+    content = RichTextUploadingField(verbose_name='Контент')
+    published = models.DateField(verbose_name='Дата публикации')
+    category = models.OneToOneField(Category, on_delete=models.CASCADE, verbose_name='Категория')
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ['-published', ]
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    brand = models.CharField(max_length=50, verbose_name='Бренд')
+    model = models.CharField(max_length=50, verbose_name='Модель')
+    # title = models.CharField(max_length=150, verbose_name='Название продукта')
+    disc = models.TextField(verbose_name='Описание')
+    price = models.IntegerField(verbose_name='Стоимость')
+    image = models.ImageField(null=True, blank=True, width_field='120', upload_to='images/', verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='products')
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ['-brand', '-model', ]
+
+    def __str__(self):
+        return f'{self.brand} {self.model}'
+
+
+
+
+
+
+
+
+
