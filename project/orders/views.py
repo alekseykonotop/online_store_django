20650a11369c06f745812d24f2732a41a238dd16
Aleksey Konotop <alekseykonotop@gmail.com>
2019-11-03 14:47:56 +0300

    Добавил запрет создания заказа, если пользователь не авторизован.

--- project/orders/views.py
+++ project/orders/views.py
@@ -6,19 +6,26 @@ from cart.cart import Cart
 
 def order_create(request):
     cart = Cart(request)
+    errors = []
     if request.method == 'POST':
-        form = OrderCreateForm(request.POST)
-        if form.is_valid():
-            order = form.save(commit=False)
-            order.user = request.user
-            order.save()
-            for item in cart:
-                OrderItem.objects.create(order=order,
-                                         product=item['product'],
-                                         price=item['price'],
-                                         quantity=item['quantity'])
-            cart.clean()
+        if request.user.is_authenticated:
+            form = OrderCreateForm(request.POST)
+            if form.is_valid():
+                order = form.save(commit=False)
+                order.user = request.user
+                order.save()
+                for item in cart:
+                    OrderItem.objects.create(order=order,
+                                             product=item['product'],
+                                             price=item['price'],
+                                             quantity=item['quantity'])
+                cart.clean()
 
-            return render(request, 'order/created.html')
+                return render(request, 'order/created.html', {'errors': errors})
+        else:
+            errors.append('Для оформления заказа необходимо авторизоваться.')
+
+        return render(request, 'cart/detail.html', {'errors': errors})
+
 
 
